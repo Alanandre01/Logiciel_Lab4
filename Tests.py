00c2abac8231fcf8ae2843db58052689771739b8
Alanandre01 <brucher.alan@gmail.com>
2021-11-09 22:42:20 -0500

    test keyword is none - fail

--- Tests.py
+++ Tests.py
@@ -1,6 +1,6 @@
 import unittest
 from Server import Database
-
+from TwitterAPI import TwitterAPI, BEARER_TOKEN
 
 class TestDatabase(unittest.TestCase):
     def setUp(self):
@@ -18,4 +18,12 @@ class TestServer(unittest.TestCase):
 
 
 class TestTwitterAPI(unittest.TestCase):
-    pass
+    def test_keyword_is_none(self):
+        headers = TwitterAPI.create_twitter_headers()
+        url, params = TwitterAPI.create_twitter_url("data")
+        params[0] = None
+        json_response = TwitterAPI.query_twitter_api(url, headers, params)
+        self.assertEqual(json_response, "Error: 'params' is empty")
+
+if __name__ == '__main__':
+    unittest.main()
